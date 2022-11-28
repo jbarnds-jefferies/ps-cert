6f234750e170d44cadd81bf2c67cdcdc0abd7f02
jbarnds-jefferies <118138706+jbarnds-jefferies@users.noreply.github.com>
2022-11-28 12:04:09 -0800

    Update sign.ps1

--- sign.ps1
+++ sign.ps1
@@ -7,6 +7,6 @@ Param (
 
 $cert = dir Cert:\CurrentUser\My\7E595AB6BADE0CF1096BBB9E17003755898A8986
 $timestamp_server = "http://timestamp.digicert.com"
+$full_path = Resolve-Path $path
 
-Set-AuthenticodeSignature $path.FullName -Certificate $cert -TimestampServer $timestamp_server
-
+Set-AuthenticodeSignature $full_path -Certificate $cert -TimestampServer $timestamp_server
